5fb7ae23d03639eb14931847d3db6640eea1c7f1
francescopapaleo <francesco.papaleo@gmail.com>
2023-05-19 18:48:07 +0200

    run.sh fixed

--- run.sh
+++ run.sh
@@ -2,13 +2,12 @@
 #SBATCH -J TCN
 #SBATCH -p short
 #SBATCH -N 1
-#SBATCH --gres=gpu:pascal:1
+#SBATCH --gres=gpu:tesla:1
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=16g
 #SBATCH --time=8:00
-#SBATCH -o %N.%J.OUTPUT.out
-#SBATCH -e %N.%J.ERROR.err
-
-module load CUDA/11.4.3
+#SBATCH -o %N.%J.OUT.out
+#SBATCH -e %N.%J.ERR.err
 
 source /etc/profile.d/lmod.sh
 source /etc/profile.d/zz_hpcnow-arch.sh
