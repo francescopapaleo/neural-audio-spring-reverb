219c51b3116990430409cb90e50c1ed78c06cf53
fra <98514696+francescopapaleo@users.noreply.github.com>
2023-05-26 17:52:31 +0200

    validation loop

--- train.py
+++ train.py
@@ -1,38 +1,51 @@
-# train.py
-import numpy as np
-import torch
-import torchsummary
-import auraloss
-import pickle
+from config import parser, model_params
+from tcn import TCN
+
+from utils.data import SpringDataset
+
 from pathlib import Path
-from matplotlib import pyplot as plt
 
-from utils.dataload import SpringDataset
-from tcn import TCN, model_params
-from utils.plot import plot_compare_waveform, plot_zoom_waveform, plot_loss_function
+import h5py
+import os
+import sys
+import tqdm
+import numpy as np
+import soundfile as sf
+import tempfile
 
-from config import parser
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import torchaudio
+import torchaudio.transforms as T
+import torchvision
+
+import auraloss
+
+import matplotlib.pyplot as plt
+
 args = parser.parse_args()
 
 torch.backends.cudnn.benchmark = True
 torch.manual_seed(args.seed)
 
 print("## Loading data...")
-full_train_set = SpringDataset(args.data_dir, split='train')
+transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
 
-# Split full_train_set into a training set and a validation set
-subset_length = 20
-val_length = subset_length  # Use the same size for validation as for training
-remainder_length = len(full_train_set) - subset_length - val_length
-train_set, val_set, _ = torch.utils.data.random_split(
-    full_train_set, [subset_length, val_length, remainder_length])
+dataset = SpringDataset(root_dir=args.data_dir, split='train')
+train_dataset = SpringDataset(root_dir=args.data_dir, split='train')
 
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=0)
-val_loader = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, shuffle=False, num_workers=0)  # Don't shuffle validation data
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+
+train, valid = torch.utils.data.random_split(dataset, [train_size, val_size])
+
+trainloader = torch.utils.data.DataLoader(train, batch_size=32)
+validloader = torch.utils.data.DataLoader(valid, batch_size=32)
 
 
-print("## Training...")
+print("## Instantiating model...")
 
+device = torch.device(args.device)
 model = TCN(
     n_inputs=1,
     n_outputs=1,
@@ -42,165 +55,98 @@ model = TCN(
     dilation_growth=model_params["dilation_growth"], 
     n_channels=model_params["n_channels"],
     )
+model = model.to(args.device)  # move the model to the right device
 
-model = model.float()  # Ensure the model uses float tensors
-
-# Receptive field and number of parameters
-rf = model.compute_receptive_field()
-params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-print(f"Parameters: {params*1e-3:0.3f} k")
-print(f"Receptive field: {rf} samples or {(rf/ args.sr)*1e3:0.1f} ms")
-
-# Initialize lists for storing losses values
-mse_loss_values = []
-l1_loss_values = []
-stft_loss_values = []
-esr_loss_values = []
-dc_loss_values = []
-snr_loss_values = []
-
-mse = torch.nn.MSELoss()
-l1 = torch.nn.L1Loss()
-stft = auraloss.freq.MultiResolutionSTFTLoss(
+# setup loss function and optimizer
+criterion = auraloss.freq.MultiResolutionSTFTLoss(
     fft_sizes=[32, 128, 512, 2048],
     win_lengths=[32, 128, 512, 2048],
-    hop_sizes=[16, 64, 256, 1024])
+    hop_sizes=[16, 64, 256, 1024]
+)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+mse = torch.nn.MSELoss()
+mse__values = []
+
 esr = auraloss.time.ESRLoss()
-dc = auraloss.time.DCLoss()
+l1__values = []
+
 snr = auraloss.time.SNRLoss()
+esr__ = []
 
-# Optimizer
-optimizer = torch.optim.Adam(model.parameters(), model_params['lr'])
-ms1 = int(args.epochs * 0.8)
-ms2 = int(args.epochs * 0.95)
-milestones = [ms1, ms2]
-print(
-    "Learning rate schedule:",
-    f"1:{model_params['lr']:0.2e} ->",
-    f"{ms1}:{model_params['lr']*0.1:0.2e} ->",
-    f"{ms2}:{model_params['lr']*0.01:0.2e}",
-)
+epochs = 10
+min_valid_loss = np.inf
+model = model.float()
 
-# Learning rate scheduler
-scheduler = torch.optim.lr_scheduler.MultiStepLR(
-    optimizer,
-    milestones,
-    gamma=0.1,
-    verbose=False,
-)
-device = torch.device(args.device)
-model.to(device)
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
 
-# Training loop
-for epoch in range(args.epochs):
-    model.train()
-    for i, (inputs, targets) in enumerate(train_loader):
-        inputs, targets = inputs.float().to(device), targets.float().to(device)
-        # ...
+global_step = 0
 
-        optimizer.zero_grad()   # zero the gradient buffers
-
-        x_batch = inputs
-        y_batch = targets
-        c = torch.tensor([0.0, 0.0]).view(1,x_batch.shape[0],-1)
-
-        # Crop input and target data
-        start_idx = rf
-        stop_idx = start_idx + model_params["length"]
-        x_crop = x_batch[..., start_idx - rf + 1 : stop_idx]
-        y_crop = y_batch[..., start_idx : stop_idx]
-        input = x_crop
-        target = y_crop
-
-        # Forward pass
-        output = model(input, c)
-
-        mse_loss = mse(output, target)
-        l1_loss = l1(output, target)      
-        stft_loss = stft(output, target)
-        esr_loss = esr(output, target)
-        dc_loss = dc(output, target)
-        snr_loss = snr(output, target)
+print('Training started...')
+model.train()
+for e in range(epochs):
+    train_loss = 0.0
+    for input, target in trainloader:
         
-        # Compute the loss
-        stft_loss.backward()         # Backward pass
-        optimizer.step()        # Update the model parameters
-        scheduler.step()        # Update the learning rate scheduler
+        input, target = input.to(args.device), target.to(args.device)
 
-        if (epoch + 1) % 1 == 0:
-                loss_info = f"Epoch {epoch+1} Training Loss: {stft_loss.item():0.3e}"
-                print(f" {loss_info} | ")
-                print("")
-                print(f"MSE loss: {mse_loss.item():0.3e}")
-                print(f"L1 loss: {l1_loss.item():0.3e}")
-                print(f"STFT loss: {stft_loss.item():0.3e}")
-                print(f"ESR loss: {esr_loss.item():0.3e}")
-                print(f"DC loss: {dc_loss.item():0.3e}")
-                print(f"SNR loss: {snr_loss.item():0.3e}")
+        # Clear the gradients
+        optimizer.zero_grad()
+        # Forward Pass
+        output = model(input.float())
+        # Find the Loss
+        loss = criterion(output, target)
+        # Calculate gradients 
+        loss.backward()
+        # Update Weights
+        optimizer.step()
+        # Calculate Loss
+        train_loss += loss.item()
+    
+        mse_loss = mse(output, target)
+        esr_loss = esr(output, target)
+        snr_loss = snr(output, target)
 
+        # Log the training loss
+        writer.add_scalar('Loss/Train', loss.item(), global_step)
+        writer.add_scalar('Loss/MSE', mse_loss.item(), global_step)
+        writer.add_scalar('Loss/ESR', esr_loss.item(), global_step)
+        writer.add_scalar('Loss/SNR', snr_loss.item(), global_step)
 
-                # Store metric values over time
-                mse_loss_values.append(mse_loss.item())
-                l1_loss_values.append(l1_loss.item())
-                stft_loss_values.append(stft_loss.item())
-                esr_loss_values.append(esr_loss.item())
-                dc_loss_values.append(dc_loss.item())
-                snr_loss_values.append(snr_loss.item())
+        # Getting the weights of the first layer of the model
+        weights = next(model.parameters()).cpu().data
+        writer.add_histogram('Weights', weights, global_step)
+        global_step += 1
+    
+    print('Validation Loop')
+    valid_loss = 0.0
+    model.eval()
+    for input, target in validloader:
 
-    model.eval()  # Set the model to evaluation mode
-    with torch.no_grad():  # Don't track gradients during validation
-        for i, (inputs, targets) in enumerate(val_loader):
-            inputs, targets = inputs.float().to(device), targets.float().to(device)
-    model.train()  # Set the model back to training mode
+        input, target = input.to(args.device), target.to(args.device)
+        
+        # Forward Pass
+        output = model(input.float(), c)
+        # Find the Loss
+        loss = criterion(output, target)
+        # Calculate Loss
+        valid_loss += loss.item()
 
-# Convert lists to numpy arrays for easier manipulation
-mse_loss_values = np.array(mse_loss_values)
-l1_loss_values = np.array(l1_loss_values)
-stft_loss_values = np.array(stft_loss_values)
-esr_loss_values = np.array(esr_loss_values)
-dc_loss_values = np.array(dc_loss_values)
-snr_loss_values = np.array(snr_loss_values)
-
-# Save the loss data
-with open('loss_data.pkl', 'wb') as f:
-    pickle.dump({
-        'mse_loss_values': mse_loss_values,
-        'l1_loss_values': l1_loss_values,
-        'stft_loss_values': stft_loss_values,
-        'esr_loss_values': esr_loss_values,
-        'dc_loss_values': dc_loss_values,
-        'snr_loss_values': snr_loss_values
-    }, f)
-
-print('Saving plots...')
-normalized_mse_loss = (mse_loss_values - np.min(mse_loss_values)) / (np.max(mse_loss_values) - np.min(mse_loss_values))
-normalized_l1_loss = (l1_loss_values - np.min(l1_loss_values)) / (np.max(l1_loss_values) - np.min(l1_loss_values))
-normalized_stft_loss = (stft_loss_values - np.min(stft_loss_values)) / (np.max(stft_loss_values) - np.min(stft_loss_values))
-normalized_esr_loss = (esr_loss_values - np.min(esr_loss_values)) / (np.max(esr_loss_values) - np.min(esr_loss_values))
-normalized_dc_loss = (dc_loss_values - np.min(dc_loss_values)) / (np.max(dc_loss_values) - np.min(dc_loss_values))
-normalized_snr_loss = (snr_loss_values - np.min(snr_loss_values)) / (np.max(snr_loss_values) - np.min(snr_loss_values))
-
-# Plotting the metrics over time
-iterations_per_epoch = len(train_loader)
-total_iterations = iterations_per_epoch * args.epochs
-time_values = np.arange(total_iterations)
+    # Print and log the training loss
+    print(f'Epoch {e+1} \t\t Training Loss: {train_loss / len(trainloader)} \t\t Validation Loss: {valid_loss / len(validloader)}')
+    
+    if min_valid_loss > valid_loss:
+        print(f'Validation loss decreased ({min_valid_loss:.6f} --> {valid_loss:.6f}).  Saving model ...''')
+        min_valid_loss = valid_loss
+        
+        save_to = Path(args.models_dir) / args.save
+        torch.save(model.state_dict(), save_to)
 
 
-plt.figure(figsize=(15, 7))
-plt.plot(time_values, normalized_mse_loss, label="MSE Loss")
-plt.plot(time_values, normalized_l1_loss, label="L1 Loss")
-plt.plot(time_values, normalized_stft_loss, label="STFT Loss")
-plt.plot(time_values, normalized_esr_loss, label="ESR Loss")
-plt.plot(time_values, normalized_dc_loss, label="DC Loss")
-plt.plot(time_values, normalized_snr_loss, label="SNR Loss")
-plt.xlabel("Epochs")
-plt.ylabel("Normalized Metric Value")
-plt.title("Training Progress: Loss Functions Over Iterations")
-plt.legend()
-plt.savefig(Path(args.results_dir) / 'eval_metrics_plot.png')
+# Close the TensorBoard writer
+writer.close()
 
-# Save the model
-save_to = Path(args.models_dir) / args.save
-torch.save(model.state_dict(), save_to)
 
+if __name__ == "__main__":
+    parser.parse_args()
---
+++ utils/data.py
@@ -0,0 +1,87 @@
+# Description: This file contains the code for the dataset class and the subset generator class.
+from pathlib import Path
+from config import parser
+
+import h5py
+import torch
+import numpy as np
+
+args = parser.parse_args()
+
+# Set seed
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+
+class SpringDataset(torch.utils.data.Dataset):
+    def __init__(self, root_dir, split=None):
+        super(SpringDataset, self).__init__()
+        self.root_dir = Path(root_dir)
+        self.split = split
+        self.seed = torch.seed()
+
+        self.file_list = list(self.root_dir.glob('**/*.h5'))
+        self.dry_file = [f for f in self.file_list if 'dry' in f.stem and self.split in f.stem][0]
+        self.wet_file = [f for f in self.file_list if 'wet' in f.stem and self.split in f.stem][0]
+        self.dry_data = None
+        self.wet_data = None
+        self.load_data()
+        self.index = {i: (self.dry_file.name, self.wet_file.name, i) for i in range(len(self.dry_data))}
+        
+    def __len__(self):
+        # The length should be the number of items in the data
+        return len(self.index)
+    
+    def __getitem__(self, index):
+        # Should return a tuple of numpy arrays (x, y )
+        x, y = self.dry_data[index], self.wet_data[index]
+        x, y = x.reshape(1, -1), y.reshape(1, -1)    
+        return x, y
+    
+    def load_data(self):
+        with h5py.File(self.dry_file, 'r') as f_dry, \
+             h5py.File(self.wet_file, 'r') as f_wet:
+            dry_key = list(f_dry.keys())[0] 
+            wet_key = list(f_wet.keys())[0] 
+            self.dry_data = f_dry[dry_key][:]
+            self.wet_data = f_wet[wet_key][:]
+
+    def print_info(self):
+        print(f"Dry File: {self.dry_file}")
+        print(f"Wet File: {self.wet_file}")
+        print(f"Dry data shape: {self.dry_data.shape}")
+        print(f"Wet data shape: {self.wet_data.shape}")
+
+        dry_samples_total = np.sum([sample.shape[0] for sample in self.dry_data])
+        wet_samples_total = np.sum([sample.shape[0] for sample in self.wet_data])
+        dry_minutes_total = dry_samples_total / 16000 / 60
+        wet_minutes_total = wet_samples_total / 16000 / 60
+        
+        print(f"Dry samples total: {dry_samples_total}")
+        print(f"Wet samples total: {wet_samples_total}")
+        print(f"Dry minutes total: {dry_minutes_total:.2f}")
+        print(f"Wet minutes total: {wet_minutes_total:.2f}")
+        
+        print("(item_n, samples_per_item, channels)")
+
+    def print_index_info(self, index):
+        """Prints the information about a specific index tuple."""
+        print(f"Index: {index}")
+        print(f"Index data: {self.index[index]}")
+
+    def load_random_subset(self, size, seed=None):
+        """Loads a random subset of the dataset."""
+        if seed is not None:
+            np.random.seed(seed)
+            torch.manual_seed(seed)
+        
+        indices = np.random.choice(len(self.dry_data), size=size, replace=False)
+        dry_subset = self.dry_data[indices]
+        wet_subset = self.wet_data[indices]
+        
+        return dry_subset, wet_subset, indices
+        
+    def concatenate_samples(self, data):
+        print(f"Shape before concatenation: {data.shape}")
+        concatenated_data = np.concatenate(data, axis=0)
+        reshape_data = concatenated_data.reshape(1, -1)      
+        return reshape_data
