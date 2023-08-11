3af35d947117f18dad83fcc18fca7d8adf264b78
francescopapaleo <francesco.papaleo@gmail.com>
2023-08-11 22:18:56 +0000

    train

--- configurations.py
+++ configurations.py
@@ -15,8 +15,8 @@ def parse_args():
     parser.add_argument('--config', type=str, default='TCN-BL', help='The configuration to use')
     parser.add_argument('--device', type=str, default=None, help='set device to run the model on')
     parser.add_argument('--sample_rate', type=int, default=44100, help='sample rate of the audio')    
-    parser.add_argument('--n_epochs', type=int, default=25, help='the total number of epochs')
-    parser.add_argument('--batch_size', type=int, default=4, help='batch size')
+    parser.add_argument('--n_epochs', type=int, default=1000, help='the total number of epochs')
+    parser.add_argument('--batch_size', type=int, default=8, help='batch size')
     parser.add_argument('--lr', type=float, default=0.0001, help='learning rate')
     # parser.add_argument('--crop', type=int, default=240000, help='crop size')
         
@@ -46,27 +46,28 @@ configs = [{
     'cond_dim': 2,
     },
     {
-    'conf_name': 'WN-8-10-2',
-    'model_type': 'WaveNet',
-    'n_channels': 8,
+    'conf_name': 'TCN-64-10-5-7',
+    'model_type': 'TCN',
+    'n_channels': 64,
     'dilation': 10,
-    'num_repeat': 2,
+    'n_blocks': 5,
+    'kernel_size': 7,
+    'cond_dim': 2,
+    },
+    {
+    'conf_name': 'WN-16-10-3',
+    'model_type': 'WaveNet',
+    'n_channels': 16,
+    'dilation': 10,
+    'num_repeat': 3,
     'kernel_size': 5,
     },
     {
-    'conf_name': 'LSTM-96',
+    'conf_name': 'LSTM-32-4',
     'model_type': 'LSTM',
     'input_size': 1,
     'output_size': 1,
-    'hidden_size': 96,
-    'num_layers': 1,
-    },
-    {
-    'conf_name': 'LSTM-96-skip',
-    'model_type': 'LSTMskip',
-    'input_size': 1,
-    'output_size': 1,
-    'hidden_size': 96,
-    'num_layers': 1,
+    'hidden_size': 32,
+    'num_layers': 4,
     },
     ]
