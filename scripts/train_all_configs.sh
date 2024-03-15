5782b810f00d0cdfdb7c1ec8d3341076f4d0b8a2
francescopapaleo <francesco.papaleo@gmail.com>
2024-03-16 00:38:30 +0100

    server backup

--- scripts/train_all_configs.sh
+++ scripts/train_all_configs.sh
@@ -10,8 +10,8 @@ configs_dir="configs/"
 for config in "$configs_dir"/*.yaml; do
     # Call your test script with the checkpoint file as an argument
     
-    # nafxsr train --init "$config" --dataset springset --sample_rate 16000 --bit_depth 16 --num_workers 16 --batch_size 64
-    nafx-springrev train --init "$config" --dataset egfxset --sample_rate 48000 --bit_depth 24 --num_workers 16 --batch_size 16
+    nafx-springrev train --init "$config" --dataset springset --sample_rate 16000 --bit_depth 16 --num_workers 16 --batch_size 64
+    # nafx-springrev train --init "$config" --dataset egfxset --sample_rate 48000 --bit_depth 24 --num_workers 16 --batch_size 16
     printf "Done.\n\n"
 
 done
